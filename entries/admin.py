1499ed402656c043a78e93284a9cf81b9d04f0fe
angelcruijff <angel.cruijff@gmail.com>
2016-01-07 23:22:10 -0600

    Se agrega interfaz admin para configuración de entries

--- entries/admin.py
+++ entries/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Entry, Metadata
 
-admin.site.register(Entry, Metadata)
+class EntryAdmin(admin.ModelAdmin):
+    empty_value_display = '-Select-'
+
+
+class MetadataAdmin(admin.ModelAdmin):
+    empty_value_display = '-Select-'
+    list_display = ('entry', 'name', 'key', 'required')
+
+
+admin.site.register(Entry, EntryAdmin)
+admin.site.register(Metadata, MetadataAdmin)
--- entries/models.py
+++ entries/models.py
@@ -5,8 +5,20 @@ class Entry(models.Model):
     name = models.CharField(max_length=100)
     template = models.TextField(blank=True)
 
+    class Meta:
+        verbose_name_plural = "Entries"
+
+    def __str__(self):
+        return self.name
+
 class Metadata(models.Model):
-    entry = models.ForeigKey(Entry)
+    entry = models.ForeignKey(Entry)
     name = models.CharField(max_length=100)
     key = models.CharField(max_length=50)
     required = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name_plural = "metadata"
+
+    def __str__(self):
+        return "{} - {}".format(name, key)
